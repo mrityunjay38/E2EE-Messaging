68c93b3ebe36002d97f51526e087d4e6060a0454
“Mrityunjay <mrityunjay38@gmail.com>
2023-09-22 22:44:20 +0530

    Basic socket setup

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+  },
+});
+
+/* Start-Enable cors + set static frontend */
+app.use(cors());
+// app.use(express.static(path.join(__dirname, "../app/build")));
+/* End-Enable cors + set static frontend */
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname, "../app/build" + "/index.html");
+// });
+
+io.on("connection", (socket) => {
+  socket.on("to_server_message", ({ message = "NA" }) => {
+    socket.broadcast.emit("client_message", { message });
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on *:3001");
+});
